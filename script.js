4205800924a0bba29cfacf361bc3baca8df1c927
chrismw <chrismw7579@gmail.com>
2020-02-04 18:29:06 -0800

    finished script file

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let humidity = document.querySelector("#humidity");
 let wind = document.querySelector("#wind");
 let UV = document.querySelector("#UV");
 let fiveDay = document.querySelectorAll(".day");
+let search = document.querySelector("#search");
+let cityIcon = document.querySelector("#city-icon");
 
 let icons = {
     clouds: 'icons/png/cloudy.png',
@@ -17,13 +19,19 @@ let icons = {
 }
 
 let apiKey = '7529f7be67780d5b335f0689ae25c356';
-let city = '';
 let lat = 0;
 let lon = 0;
 
+Getcities();
+
 submit.addEventListener('click', function() {
-    city = input.value;
     
+    GetData(input.value);
+    
+});
+
+function GetData(city) {
+
     url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=imperial`;
     fetch(url)
     .then(function(response) {
@@ -33,7 +41,7 @@ submit.addEventListener('click', function() {
         
         if (data.cod == 200) {
 
-            let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city},us&APPID=${apiKey}&units=imperial`;
+            let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}&units=imperial`;
         
             fetch(url)
             .then(function(response) {
@@ -55,51 +63,115 @@ submit.addEventListener('click', function() {
                 DisplayUV(data);
             })
         }
-        
     })
-});
+}
 
+navigator.geolocation.getCurrentPosition(GetLocation);
 
+function GetLocation(position) {
+   let lat = position.coords.latitude;
+   let lon = position.coords.longitude;
+   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${apiKey}`;
+   fetch(url)
+   .then(function(response) {
+       return response.json();
+   })
+   .then(function(data){
+       if (localStorage.getItem(data.name) == null) {
+            GetData(data.name);
+       } else {
+            GetData(input.value);
+       }
+   })
+}
 
 function DisplayUV(data) {
     UV.textContent = "UV Index: " + data.value;
 }
 
-
 function DisplayCity(data) {
-    name.textContent = data.name;
-    
-    temp.textContent = "Tempurature: " + data.main.temp + " F";
+    console.log(data);
+    var today = new Date();
+    var dd = String(today.getDate());
+    var mm = String(today.getMonth() + 1); 
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+
+    name.textContent = `${data.name} (${today})`;
+    cityIcon.setAttribute('src', SetImage(data.weather[0]));
+    temp.textContent = "Tempurature: " + data.main.temp + " \u00B0F";
     humidity.textContent = "Humidity: " + data.main.humidity;
-    wind.textContent =  "Wind Speed: " + data.wind.speed + " mph";
+    wind.textContent =  "Wind Speed: " + data.wind.speed + " MPH";
     lat = data.coord.lat;
     lon = data.coord.lon;
+    localStorage.setItem(data.name, data.name);
+    input.value = data.name;
+    Getcities();
+}
+
+
+
+function Getcities() {
+    
+    let list = document.querySelector('#list');
+    list.innerHTML = '';
+    keys = Object.keys(localStorage);
+    for (var i  = 0; i < keys.length; i++){
+        btn = document.createElement("div");
+        btn.setAttribute('class', 'previous');
+        btn.setAttribute('data-city', keys[i]);
+        
+        btn.textContent = keys[i];
+        // btn.addEventListener('click', function() {
+
+        //     console.log(btn.getAttribute('data-city'));
+        //     GetData(btn.getAttribute('data-city'));
+            
+        // })
+        
+        list.appendChild(btn);
+    } 
+    let prev = document.getElementsByClassName('previous');
+    createPreviousButtons(prev);
+}
+
+
+function createPreviousButtons(datas) {
+
+    for (let index = 0; index < datas.length; index++) {
+        datas[index].addEventListener('click', function() {
+            GetData(datas[index].getAttribute('data-city')); 
+        })
+    }
 }
 
 function DisplayFiveDay(data) {
     
     for (var i = 0; i < data.list.length; i+=8) {
         fiveDay[i / 8].querySelector(".date").textContent = data.list[i].dt_txt.split(" ")[0];
-        fiveDay[i / 8].querySelector(".temp").textContent = "Temp: " + data.list[i].main.temp + " F";
+        fiveDay[i / 8].querySelector(".temp").textContent = "Temp: " + data.list[i].main.temp + " \u00B0F";
         fiveDay[i / 8].querySelector(".humid").textContent = "Humidity: " + data.list[i].main.humidity + "%";
-        
-        let image = '';
-        let description = data.list[i].weather[0].main;
-        if (description == 'Rain') {
-            image = icons.Rain;
-        } else if (description == 'Clouds') {
-            image = icons.clouds;
-            let clouds = data.list[i].weather[0].description.split(" ");
-            if (clouds[0] == 'broken' || clouds[0] == 'few' || clouds[0] == 'scattered') {
-                image = icons.partyCloudy;
-            }
-        } else if (description == 'Clear') {
-            image = icons.sunny;
-        } else if (description[1] == 'Snow') {
-            image = icons.snowy;
-        } else if (description == 'Thunderstorm') {
-            image = icons.thunder;
-        }
-        fiveDay[i / 8].querySelector("img").setAttribute('src', image)
+        fiveDay[i / 8].querySelector("img").setAttribute('src', SetImage(data.list[i].weather[0]));
     }
+}
+
+function SetImage(data) {
+    let image = '';
+    let description = data.main;
+    if (description == 'Rain') {
+        image = icons.Rain;
+    } else if (description == 'Clouds') {
+        image = icons.clouds;
+        let clouds = data.description.split(" ");
+        if (clouds[0] == 'broken' || clouds[0] == 'few' || clouds[0] == 'scattered') {
+            image = icons.partyCloudy;
+        }
+    } else if (description == 'Clear') {
+        image = icons.sunny;
+    } else if (description == 'Snow') {
+        image = icons.snowy;
+    } else if (description == 'Thunderstorm') {
+        image = icons.thunder;
+    }
+    return image;
 }
